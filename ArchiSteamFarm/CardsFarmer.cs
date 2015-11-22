b89d2977f89b99231a9dc29f4ed3a9dbdc9584ec
JustArchi <JustArchi@JustArchi.net>
2015-11-22 01:45:58 +0100

    10 hours should be enough

--- ArchiSteamFarm/CardsFarmer.cs
+++ ArchiSteamFarm/CardsFarmer.cs
@@ -33,7 +33,7 @@ using System.Threading.Tasks;
 namespace ArchiSteamFarm {
 	internal class CardsFarmer {
 		private const byte StatusCheckSleep = 5; // In minutes, how long to wait before checking the appID again
-		private const ushort MaxFarmingTime = 300; // In minutes, how long ASF is allowed to farm one game in solo mode
+		private const ushort MaxFarmingTime = 600; // In minutes, how long ASF is allowed to farm one game in solo mode
 
 		private readonly ManualResetEvent FarmResetEvent = new ManualResetEvent(false);
 		private readonly SemaphoreSlim Semaphore = new SemaphoreSlim(1);
