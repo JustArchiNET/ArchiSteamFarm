21e892c04209519c20b9dddd059572ae78b7f8de
JustArchi <JustArchi@JustArchi.net>
2015-12-22 18:24:26 +0100

    Misc

--- ArchiSteamFarm/ArchiWebHandler.cs
+++ ArchiSteamFarm/ArchiWebHandler.cs
@@ -181,7 +181,7 @@ namespace ArchiSteamFarm {
 			bool? isLoggedIn = await IsLoggedIn().ConfigureAwait(false);
 			if (isLoggedIn.HasValue && !isLoggedIn.Value) {
 				Logging.LogGenericInfo(Bot.BotName, "Reconnecting because our sessionID expired!");
-				await Bot.Restart().ConfigureAwait(false);
+				var restart = Task.Run(async () => await Bot.Restart().ConfigureAwait(false));
 				return true;
 			}
 
