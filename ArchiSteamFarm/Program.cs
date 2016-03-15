6213e77fab60febfbc76d485093058f606916d88
JustArchi <JustArchi@JustArchi.net>
2016-03-15 07:09:30 +0100

    Time to sleep

--- ArchiSteamFarm/Program.cs
+++ ArchiSteamFarm/Program.cs
@@ -397,7 +397,7 @@ namespace ArchiSteamFarm {
 						Logging.LogGenericInfo("Command sent: " + arg);
 
 						// We intentionally execute this async block synchronously
-						Logging.LogGenericInfo("Response received: " + WCF.SendCommand(arg);
+						Logging.LogGenericInfo("Response received: " + WCF.SendCommand(arg));
 						/*
 						Task.Run(async () => {
 							Logging.LogGenericNotice("WCF", "Response received: " + await WCF.SendCommand(arg).ConfigureAwait(false));
