cd462f607e888b1ee83bb0c36572382344ec6273
JustArchi <JustArchi@JustArchi.net>
2016-07-03 00:46:16 +0200

    Move to Fody.Costura for repacking This should be the best way for both Windows and Mono, plus is much more cleaner, get rid of those crappy tools

---
+++ packages/Fody.1.30.0-beta01/Tools/install.ps1
@@ -0,0 +1,3 @@
+﻿param($installPath, $toolsPath, $package, $project)
+$item = $project.ProjectItems | where-object {$_.Name -eq "FodyWeavers.xml"} 
+$item.Properties.Item("BuildAction").Value = [int]0
