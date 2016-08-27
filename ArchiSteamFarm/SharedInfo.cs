3814bd64f0025f08bbf687f575e97d78f99ec9b7
JustArchi <JustArchi@JustArchi.net>
2016-08-27 10:15:11 +0200

    Bump

--- ArchiSteamFarm/SharedInfo.cs
+++ ArchiSteamFarm/SharedInfo.cs
@@ -41,7 +41,7 @@ namespace ArchiSteamFarm {
 			WCFHostname
 		}
 
-		internal const string VersionNumber = "2.1.4.5";
+		internal const string VersionNumber = "2.1.4.6";
 		internal const string Copyright = "Copyright © ArchiSteamFarm 2015-2016";
 
 		internal const string GithubRepo = "JustArchi/ArchiSteamFarm";
