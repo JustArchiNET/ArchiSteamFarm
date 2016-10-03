cf35d72d9964ba4e3f2c47ef454cce50cf17e8cc
Scott Cameron <Blackpanther@panthers-den.com>
2016-10-03 16:23:30 -0400

    Setup of my own version number to allow easier merging of JustArchi code into my own without messing up my versions.

--- ArchiSteamFarm/SharedInfo.cs
+++ ArchiSteamFarm/SharedInfo.cs
@@ -41,7 +41,7 @@ namespace ArchiSteamFarm {
 			WCFHostname
 		}
 
-		internal const string VersionNumber = "2.1.5.5";
+		internal const string MyVersionNumber = "2.1.5.5";
 		internal const string Copyright = "Copyright © ArchiSteamFarm 2015-2016";
 
 		internal const string GithubRepo = "Blackpanther989/ArchiSteamFarm";
