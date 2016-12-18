8d89095670ac4adeaaacd795eae3dd2dc991d9d1
JustArchi <JustArchi@JustArchi.net>
2016-12-18 16:10:01 +0100

    Misc

--- ArchiSteamFarm/CardsFarmer.cs
+++ ArchiSteamFarm/CardsFarmer.cs
@@ -766,11 +766,11 @@ namespace ArchiSteamFarm {
 			}
 
 			public override bool Equals(object obj) {
-				if (ReferenceEquals(null, obj)) {
+				if (obj == null) {
 					return false;
 				}
 
-				if (ReferenceEquals(this, obj)) {
+				if (obj == this) {
 					return true;
 				}
 
