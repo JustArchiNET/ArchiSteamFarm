7864a8632419fb9a904ff4ea173ae2f7c7d6e03d
JustArchi <JustArchi@JustArchi.net>
2017-03-11 17:31:25 +0100

    crowdin-cli 2.0.11

--- tools/crowdin-cli/crowdin.bat
+++ tools/crowdin-cli/crowdin.bat
@@ -1,2 +1,2 @@
 @echo off 
-IF "%CROWDIN_HOME%"=="" (ECHO crowdin is NOT defined) ELSE (java -jar %CROWDIN_HOME%\crowdin-cli.jar %*)
+IF "%CROWDIN_HOME%"=="" (ECHO crowdin is NOT defined) ELSE (java -jar "%CROWDIN_HOME%\crowdin-cli.jar" %*)
