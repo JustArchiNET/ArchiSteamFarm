337dc4b1694b97d02ca008e0ec67e3d8bf7ddbc8
JustArchi <JustArchi@JustArchi.net>
2017-09-23 05:51:10 +0200

    Crowdin: Misc

--- tools/crowdin-cli/archi_download.bat
+++ tools/crowdin-cli/archi_download.bat
@@ -2,4 +2,7 @@
 pushd %~dp0
 cd ..\\..
 call crowdin -b master --identity tools\\crowdin-cli\\crowdin_identity.yaml download
+git reset
+git add -A "*.resx"
+git commit -m "Translations update"
 pause
--- tools/crowdin-cli/archi_sync.bat
+++ tools/crowdin-cli/archi_sync.bat
@@ -3,4 +3,7 @@ pushd %~dp0
 cd ..\\..
 call crowdin -b master --identity tools\\crowdin-cli\\crowdin_identity.yaml upload sources
 call crowdin -b master --identity tools\\crowdin-cli\\crowdin_identity.yaml download
+git reset
+git add -A "*.resx"
+git commit -m "Translations update"
 pause
