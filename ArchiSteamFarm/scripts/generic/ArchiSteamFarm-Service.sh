5c737a286b83cf84fbab056057a3bfb30044a40f
JustArchi <JustArchi@JustArchi.net>
2017-10-07 18:28:57 +0200

    Misc

--- ArchiSteamFarm/scripts/generic/ArchiSteamFarm-Service.sh
+++ ArchiSteamFarm/scripts/generic/ArchiSteamFarm-Service.sh
@@ -2,6 +2,8 @@
 set -eu
 cd "$(dirname "$(readlink -f "$0")")"
 
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
+
 while [[ -f ArchiSteamFarm.dll ]]; do
 	dotnet ArchiSteamFarm.dll --service # We will abort the script if ASF exits with an error
 	sleep 1
