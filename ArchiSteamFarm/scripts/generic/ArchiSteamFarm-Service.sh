128b6d2e5bdbd1007414d64f420d5c891b3a327b
JustArchi <JustArchi@JustArchi.net>
2017-10-14 11:32:47 +0200

    Add stdin handling for generic scripts

--- ArchiSteamFarm/scripts/generic/ArchiSteamFarm-Service.sh
+++ ArchiSteamFarm/scripts/generic/ArchiSteamFarm-Service.sh
@@ -12,8 +12,15 @@ ASF_ARGS+=" $*"
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 while :; do
-	dotnet ArchiSteamFarm.dll $ASF_ARGS & # We need to start ASF in the background for trap to work
-	wait $! # This will forward dotnet error code, set -e will abort the script if it's non-zero
+	if grep -Fq '"Headless": true' 'config/ASF.json'; then
+		# We're running ASF in headless mode so we don't need STDIN
+		dotnet ArchiSteamFarm.dll $ASF_ARGS & # Start ASF in the background, trap will work properly due to non-blocking call
+		wait $! # This will forward dotnet error code, set -e will abort the script if it's non-zero
+	else
+		# We're running ASF in non-headless mode, so we need STDIN to be operative
+		dotnet ArchiSteamFarm.dll $ASF_ARGS # Start ASF in the foreground, trap sadly won't work until process exit
+	fi
+
 	chmod +x "$0" # If ASF exited by itself, we need to ensure that our script is still set to +x after auto-update
 	sleep 1
 done
