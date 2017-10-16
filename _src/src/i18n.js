b26203cb8c1de4e476a23e0a7e01742d2a8faadb
Indexyz <jiduye@gmail.com>
2017-10-16 00:01:09 +0000

    Format Code

--- _src/src/i18n.js
+++ _src/src/i18n.js
@@ -1,55 +1,55 @@
 const defaultLocale = "en";
 
 export default {
-    getLanguage() {
-        // Chrome and Firefox 32+
-        return navigator.languages || [
-          navigator.language ||
-          navigator.browserLanguage
-        ]
-    },
-    loadLocale(locale) {
-        return require(`./locale/${locale}`);
-    },
-    getSplitedLanguage(language) {
-        if (language.indexOf("-") != -1) {
-            const splitedLang = language.substring(0, language.indexOf("-"))
-            return splitedLang.toLowerCase();
-        }
-        return language
-    },
-    getLocale() {
-        const messages = {};
-
-        let locale = defaultLocale;
-
-        for (const lang of this.getLanguage()) {
-            try {
-                locale = lang.toLowerCase();
-                messages[locale] = this.loadLocale(locale);
-                // First match
-                break;
-            // If import error will throw error
-            } catch (e) {
-                locale = defaultLocale; 
-            }
-        }
-
-        // match head language when first match is failure
-        if (locale === defaultLocale) {
-            for (const lang of this.getLanguage()) {
-            const splitedLang = this.getSplitedLanguage(lang);
-                try {
-                    locale = splitedLang;
-                    messages[locale] = this.loadLocale(locale);
-                    break;
-                } catch (e) {
-                    locale = defaultLocale;
-                }
-            }
-        }
-        messages[defaultLocale] = this.loadLocale(defaultLocale);
-       
-        return { messages, locale };
+  getLanguage() {
+    // Chrome and Firefox 32+
+    return navigator.languages || [
+      navigator.language ||
+      navigator.browserLanguage
+    ]
+  },
+  loadLocale(locale) {
+    return require(`./locale/${locale}`);
+  },
+  getSplitedLanguage(language) {
+    if (language.indexOf("-") != -1) {
+      const splitedLang = language.substring(0, language.indexOf("-"))
+      return splitedLang.toLowerCase();
     }
+    return language
+  },
+  getLocale() {
+    const messages = {};
+
+    let locale = defaultLocale;
+
+    for (const lang of this.getLanguage()) {
+      try {
+        locale = lang.toLowerCase();
+        messages[locale] = this.loadLocale(locale);
+        // First match
+        break;
+      // If import error will throw error
+      } catch (e) {
+        locale = defaultLocale; 
+      }
+    }
+
+    // match head language when first match is failure
+    if (locale === defaultLocale) {
+      for (const lang of this.getLanguage()) {
+        const splitedLang = this.getSplitedLanguage(lang);
+        try {
+          locale = splitedLang;
+          messages[locale] = this.loadLocale(locale);
+          break;
+        } catch (e) {
+          locale = defaultLocale;
+        }
+      }
+    }
+    messages[defaultLocale] = this.loadLocale(defaultLocale);
+       
+    return { messages, locale };
+  }
 }
