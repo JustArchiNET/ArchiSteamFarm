feafd16269ab941fed8a45eefd7421bd2763e332
JustArchi <JustArchi@JustArchi.net>
2017-10-19 12:46:42 +0200

    Add handling of --path to headless detection

--- ArchiSteamFarm/scripts/generic/ArchiSteamFarm-Service.sh
+++ ArchiSteamFarm/scripts/generic/ArchiSteamFarm-Service.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -eu
+
+CONFIG_PATH="config/ASF.json"
+
 cd "$(dirname "$(readlink -f "$0")")"
 
 if [[ -z "${ASF_ARGS-}" ]]; then
@@ -8,11 +11,17 @@ fi
 
 ASF_ARGS+=" $*"
 
+for ARG in $ASF_ARGS; do
+	case "$ARG" in
+		--path=*) CONFIG_PATH="$(echo "$ARG" | cut -d '=' -f 2-)/${CONFIG_PATH}" ;;
+	esac
+done
+
 # Kill underlying ASF process on shell process exit
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM
 
 while :; do
-	if grep -Eq '"Headless":\s+?true' 'config/ASF.json'; then
+	if [[ -f "$CONFIG_PATH" ]] && grep -Eq '"Headless":\s+?true' "$CONFIG_PATH"; then
 		# We're running ASF in headless mode so we don't need STDIN
 		dotnet ArchiSteamFarm.dll $ASF_ARGS & # Start ASF in the background, trap will work properly due to non-blocking call
 		wait $! # This will forward dotnet error code, set -e will abort the script if it's non-zero
--- run.sh
+++ run.sh
@@ -4,6 +4,8 @@ set -eu
 PROJECT="ArchiSteamFarm"
 OUT="out/source"
 
+CONFIG_PATH="config/ASF.json"
+
 cd "$(dirname "$(readlink -f "$0")")"
 
 if [[ -z "${ASF_ARGS-}" ]]; then
@@ -11,6 +13,12 @@ if [[ -z "${ASF_ARGS-}" ]]; then
 fi
 
 ASF_ARGS+=" $*"
+
+for ARG in $ASF_ARGS; do
+	case "$ARG" in
+		--path=*) CONFIG_PATH="$(echo "$ARG" | cut -d '=' -f 2-)/${CONFIG_PATH}" ;;
+	esac
+done
 
 # Kill underlying ASF process on shell process exit
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM
@@ -22,11 +30,13 @@ fi
 
 dotnet --info
 
-if grep -Eq '"Headless":\s+?true' "${PROJECT}/${OUT}/config/ASF.json"; then
+cd "${PROJECT}/${OUT}"
+
+if [[ -f "$CONFIG_PATH" ]] && grep -Eq '"Headless":\s+?true' "$CONFIG_PATH"; then
 	# We're running ASF in headless mode so we don't need STDIN
-	dotnet exec "${PROJECT}/${OUT}/${PROJECT}.dll" $ASF_ARGS & # Start ASF in the background, trap will work properly due to non-blocking call
+	dotnet exec "${PROJECT}.dll" $ASF_ARGS & # Start ASF in the background, trap will work properly due to non-blocking call
 	wait $! # This will forward dotnet error code, set -e will abort the script if it's non-zero
 else
 	# We're running ASF in non-headless mode, so we need STDIN to be operative
-	dotnet exec "${PROJECT}/${OUT}/${PROJECT}.dll" $ASF_ARGS # Start ASF in the foreground, trap won't work until process exit
+	dotnet exec "${PROJECT}.dll" $ASF_ARGS # Start ASF in the foreground, trap won't work until process exit
 fi
