5dfa55b6e429651237d0fda2622d8f00c89f3855
MrBurrBurr <mail@FREDERICK.win>
2018-02-14 03:41:46 +0100

    Misc

--- ArchiSteamFarm/www/js/app.js
+++ ArchiSteamFarm/www/js/app.js
@@ -305,6 +305,102 @@ function sendCommand() {
 //}
 //#endregion New stuff
 
+//#region Newer stuff
+//const cachedTypeDefinitions = new Map();
+//const cachedStructureDefinitions = new Map();
+
+//const API = {
+//    get: (endpoint, data) => request('GET', `/Api/${endpoint}`, data).then(extract('Result')),
+//    post: (endpoint, data) => request('POST', `/Api/${endpoint}`, data).then(extract('Result'))
+//};
+
+//const subtypeRegex = /\[[^\]]+\]/g;
+
+//function resolveSubtypes(type) {
+//    return type.match(subtypeRegex).map(subtype => subtype.slice(1, subtype.length - 1));
+//}
+
+//async function getStructureDefinition(type) {
+//    if (cachedStructureDefinitions.has(type)) return cachedStructureDefinitions.get(type);
+
+//    const structureDefinition = API.get(`Structure/${encodeURIComponent(type)}`);
+//    cachedStructureDefinitions.set(type, structureDefinition);
+
+//    return structureDefinition;
+//}
+
+//async function getTypeDefinition(type) {
+//    if (cachedTypeDefinitions.has(type)) return cachedTypeDefinitions.get(type);
+
+//    const typeDefinition = API.get(`Type/${encodeURIComponent(type)}`);
+//    cachedTypeDefinitions.set(type, typeDefinition);
+
+//    return typeDefinition;
+//}
+
+//async function resolveType(type) {
+//    switch (type.split('`')[0]) {
+//        case 'System.Boolean':
+//            return { type: 'boolean' };
+//        case 'System.String':
+//            return { type: 'string' };
+//        case 'System.Byte':
+//            return { type: 'smallNumber' };
+//        case 'System.UInt32':
+//            return { type: 'number' };
+//        case 'System.Collections.Generic.HashSet':
+//            const [subtype] = resolveSubtypes(type);
+//            return { type: 'hashSet', values: await resolveType(subtype) };
+//        case 'System.UInt64':
+//            return { type: 'bigNumber' };
+//        case 'System.Collections.Generic.Dictionary':
+//            const subtypes = resolveSubtypes(type);
+//            return { type: 'dictionary', key: await resolveType(subtypes[0]), value: await resolveType(subtypes[1]) };
+//        default: // Complex type
+//            return unwindType(type);
+//    }
+//}
+
+//async function unwindObject(type, typeDefinition) {
+//    const resolvedStructure = {
+//        type: 'object',
+//        body: {}
+//    };
+
+//    const [structureDefinition, resolvedTypes] = await Promise.all([
+//        getStructureDefinition(type),
+//        Promise.all(Object.keys(typeDefinition.Body).map(async param => ({ param, type: await resolveType(typeDefinition.Body[param]) })))
+//    ]);
+
+//    for (const { param, type } of resolvedTypes) {
+//        const paramName = typeDefinition.Body[param] !== 'System.UInt64' ? param : `s_${param}`;
+
+//        resolvedStructure.body[param] = {
+//            defaultValue: structureDefinition[param],
+//            paramName,
+//            ...type
+//        };
+//    }
+
+//    return resolvedStructure;
+//}
+
+//async function unwindType(type) {
+//    if (type === 'ArchiSteamFarm.BotConfig') getStructureDefinition(type); // Dirty trick, but 30% is 30%
+//    const typeDefinition = await getTypeDefinition(type);
+
+//    switch (typeDefinition.Properties.BaseType) {
+//        case 'System.Object':
+//            return unwindObject(type, typeDefinition);
+//        case 'System.Enum':
+//            return { type: (typeDefinition.Properties.CustomAttributes || []).includes('System.FlagsAttribute') ? 'flag' : 'enum', values: typeDefinition.Body };
+//        default:
+//            const structureDefinition = await getStructureDefinition(type);
+//            return { type: 'unknown', typeDefinition, structureDefinition };
+//    }
+//}
+//#endregion Newer stuff
+
 function generateConfigChangerHTML() {
     $.ajax({
         url: "/Api/Type/ArchiSteamFarm.BotConfig",
