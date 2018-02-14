ca1fcf33d8b405a7364d20c91adff7eede29ed63
JustArchi <JustArchi@JustArchi.net>
2018-02-14 16:33:57 +0100

    Misc

--- ArchiSteamFarm/Json/Steam.cs
+++ ArchiSteamFarm/Json/Steam.cs
@@ -23,7 +23,6 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using ArchiSteamFarm.Localization;
 using HtmlAgilityPack;
 using Newtonsoft.Json;
 using SteamKit2;
