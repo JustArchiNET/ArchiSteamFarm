b9d7dfd1aadff8c896b340828535d4d76ae2eb8b
JustArchi <JustArchi@JustArchi.net>
2018-03-20 08:02:33 +0100

    Update my crowdin scripts

--- tools/crowdin-cli/archi_download.bat
+++ tools/crowdin-cli/archi_download.bat
@@ -3,7 +3,6 @@ pushd %~dp0
 cd ..\\..
 call crowdin -b master --identity tools\\crowdin-cli\\crowdin_identity.yaml download
 git reset
-git add -A "ArchiSteamFarm\Localization\*.resx"
-git add -A "docs\WebConfigGenerator\src\locale\*.json"
+git add -A "ArchiSteamFarm\Localization\*.resx" "ArchiSteamFarm\www\locale\*.json" "docs\WebConfigGenerator\src\locale\*.json" 
 git commit -m "Translations update"
 pause
