8f8e32d503f22c40b0fcd0ca0e0e2f881c5b9b6a
JustArchi <JustArchi@JustArchi.net>
2018-03-20 08:03:47 +0100

    Misc

--- tools/crowdin-cli/archi_sync.bat
+++ tools/crowdin-cli/archi_sync.bat
@@ -4,7 +4,6 @@ cd ..\\..
 call crowdin -b master --identity tools\\crowdin-cli\\crowdin_identity.yaml upload sources
 call crowdin -b master --identity tools\\crowdin-cli\\crowdin_identity.yaml download
 git reset
-git add -A "ArchiSteamFarm\Localization\*.resx" "ArchiSteamFarm\www\locale\*.json" "docs\WebConfigGenerator\src\locale\*.json" 
-git add -A "docs\WebConfigGenerator\src\locale\*.json"
+git add -A "ArchiSteamFarm\Localization\*.resx" "ArchiSteamFarm\www\locale\*.json" "docs\WebConfigGenerator\src\locale\*.json"
 git commit -m "Translations update"
 pause
