693ac9252f15fd340a27a65d1c53c98c5122a156
JustArchi <JustArchi@JustArchi.net>
2018-04-27 05:17:13 +0200

    Follow-up

--- WebConfigGenerator/cc.sh
+++ WebConfigGenerator/cc.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 set -eu
 
-OUTPUT="dist" # Relative to script directory
 TARGET="../docs" # Relative to script directory
-SRC="../WebConfigGenerator" # Path between $OUTPUT and $TARGET
 
 cd "$(dirname "$(readlink -f "$0")")"
 
@@ -12,15 +10,7 @@ git pull
 npm install
 npm run build
 
-while read -r FILE; do
-	rm -f "$FILE"
-done < <(find "$TARGET" -mindepth 1 -maxdepth 1 -type l)
-
-while read -r FILE; do
-	ln -s "${SRC}/${FILE}" "$TARGET"
-done < <(find "$OUTPUT" -mindepth 1 -maxdepth 1)
-
 git reset
-git add -A -f "$OUTPUT" "$TARGET"
+git add -A -f "$TARGET"
 git commit -m "WebConfigGenerator build"
 git push
