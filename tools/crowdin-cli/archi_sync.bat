a68a55bb43c179ef59ad919fd70e287a1869bb93
JustArchi <JustArchi@JustArchi.net>
2018-05-08 10:53:36 +0200

    One more crowdin update

--- tools/crowdin-cli/archi_sync.bat
+++ tools/crowdin-cli/archi_sync.bat
@@ -1,6 +1,13 @@
 @echo off
 pushd %~dp0
 cd ..\\..
+
+cd wiki
+git reset --hard
+git clean -fd
+git pull
+cd ..
+
 call crowdin -b master --identity tools\\crowdin-cli\\crowdin_identity.yml upload sources
 
 call crowdin -b master --identity tools\\crowdin-cli\\crowdin_identity.yml download
--- tools/crowdin-cli/archi_upload.bat
+++ tools/crowdin-cli/archi_upload.bat
@@ -1,5 +1,12 @@
 @echo off
 pushd %~dp0
 cd ..\\..
+
+cd wiki
+git reset --hard
+git clean -fd
+git pull
+cd ..
+
 call crowdin -b master --identity tools\\crowdin-cli\\crowdin_identity.yml upload sources
 pause
