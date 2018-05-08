f4a603a606c492ac267fc278887dfbdc03d6b8ee
JustArchi <JustArchi@JustArchi.net>
2018-05-08 09:50:50 +0200

    Update crowdin scripts for wiki

--- tools/crowdin-cli/archi_sync.bat
+++ tools/crowdin-cli/archi_sync.bat
@@ -2,8 +2,18 @@
 pushd %~dp0
 cd ..\\..
 call crowdin -b master --identity tools\\crowdin-cli\\crowdin_identity.yml upload sources
+
 call crowdin -b master --identity tools\\crowdin-cli\\crowdin_identity.yml download
 git reset
-git add -A "ArchiSteamFarm\Localization\*.resx" "ArchiSteamFarm\www\locale\*.json" "WebConfigGenerator\src\locale\*.json"
+
+cd wiki
+git reset
+git add -A "locale\*.md"
 git commit -m "Translations update"
+cd ..
+
+git add -A "ArchiSteamFarm\Localization\*.resx" "ArchiSteamFarm\www\locale\*.json" "WebConfigGenerator\src\locale\*.json" "wiki"
+git commit -m "Translations update"
+
+git push --recurse-submodules=on-demand
 pause
