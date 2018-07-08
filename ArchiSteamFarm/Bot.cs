9cb348529f6596135e53886ec111ffea8e740eec
Abrynos <6608231+Abrynos@users.noreply.github.com>
2018-07-08 13:51:30 +0200

    Implement #841 (#843)
    * Move scrollable-menu's up by their height where there's always space for them to be displayed - Implements #805
    
    * Implement #841
    
    * Revert Implementation of #508 for this branch
    
    * Add if-clause to avoid unintended regex-matches
    Tabify Bot.cs
    
    * Use StringComparison and Stick to Archis convention regarding curly brackets
    
    * Remove unnecessary convertion to List and stick to Archis convention regarding \n
    
    * Make regex-prefix disappear; remove empty line
    
    * remove tabs created by VS -.-

--- ArchiSteamFarm/Bot.cs
+++ ArchiSteamFarm/Bot.cs
@@ -540,6 +540,10 @@ namespace ArchiSteamFarm {
 							continue;
 						}
 					}
+				} else if (botName.StartsWith("r!", StringComparison.OrdinalIgnoreCase)) {
+					string botPattern = botName.Substring(2);
+					IEnumerable<Bot> regexMatches = Bots.Where(kvp => Regex.Match(kvp.Key, botPattern, RegexOptions.IgnoreCase).Success).Select(kvp => kvp.Value);
+					result.UnionWith(regexMatches);
 				}
 
 				if (!Bots.TryGetValue(botName, out Bot targetBot)) {
