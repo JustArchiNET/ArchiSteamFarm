526a38a84085bbb00d5e6033e42c7f89e8fad434
Vital7 <vitalyachuma@rambler.ru>
2018-10-16 00:13:56 +0300

    Add TargetBotNotConnected string (#919)
    * Add TargetBotNotConnected string
    
    * Fix encoding

--- ArchiSteamFarm/Commands.cs
+++ ArchiSteamFarm/Commands.cs
@@ -3,16 +3,16 @@
 //   / _ \  | '__|/ __|| '_ \ | |\___ \ | __|/ _ \ / _` || '_ ` _ \ | |_  / _` || '__|| '_ ` _ \
 //  / ___ \ | |  | (__ | | | || | ___) || |_|  __/| (_| || | | | | ||  _|| (_| || |   | | | | | |
 // /_/   \_\|_|   \___||_| |_||_||____/  \__|\___| \__,_||_| |_| |_||_|   \__,_||_|   |_| |_| |_|
-// 
+//
 // Copyright 2015-2018 Łukasz "JustArchi" Domeradzki
 // Contact: JustArchi@JustArchi.net
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 // http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -632,7 +632,7 @@ namespace ArchiSteamFarm {
 			}
 
 			if (!targetBot.IsConnectedAndLoggedOn) {
-				return FormatBotResponse(Strings.BotNotConnected);
+				return FormatBotResponse(Strings.TargetBotNotConnected);
 			}
 
 			(bool success, string output) = await Bot.Actions.SendTradeOffer(appID, contextID, targetBot.SteamID).ConfigureAwait(false);
@@ -2128,7 +2128,7 @@ namespace ArchiSteamFarm {
 			}
 
 			if (!targetBot.IsConnectedAndLoggedOn) {
-				return FormatBotResponse(Strings.BotNotConnected);
+				return FormatBotResponse(Strings.TargetBotNotConnected);
 			}
 
 			if (targetBot.SteamID == Bot.SteamID) {
@@ -2171,7 +2171,7 @@ namespace ArchiSteamFarm {
 			}
 
 			if (!targetBot.IsConnectedAndLoggedOn) {
-				return FormatBotResponse(Strings.BotNotConnected);
+				return FormatBotResponse(Strings.TargetBotNotConnected);
 			}
 
 			if (targetBot.SteamID == Bot.SteamID) {
