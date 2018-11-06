5471394bab2c29f476eea2e55ba24c96311410ed
Łukasz Domeradzki <JustArchi@JustArchi.net>
2018-11-06 06:00:01 +0100

    Implement automatic crowdin strings upload in AppVeyor (#937)
    * Initial crowdin-ci integration
    
    * Fix syntax
    
    * Fix detached heads
    
    * Fix syntax for java-only installs
    
    * Finish the PR
    
    * Misc

--- tools/crowdin-cli/archi_download.ps1
+++ tools/crowdin-cli/archi_download.ps1
@@ -2,32 +2,6 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 $ProgressPreference = 'SilentlyContinue'
 
-Set-Location $PSScriptRoot
-Set-Location ..\\..
-
-crowdin -b master --identity tools\\crowdin-cli\\crowdin_identity.yml download
-git reset
-
-Push-Location -Path ASF-ui
-git pull
-git add -A "src\i18n\locale\*.json"
-git commit -m "Translations update"
-Pop-Location
-
-Push-Location -Path ASF-WebConfigGenerator
-git pull
-git add -A "src\locale\*.json"
-git commit -m "Translations update"
-Pop-Location
-
-Push-Location -Path wiki
-git pull
-git add -A "locale\*.md"
-git commit -m "Translations update"
-Pop-Location
-
-git add -A "ArchiSteamFarm\Localization\*.resx" "ASF-ui" "ASF-WebConfigGenerator" "wiki"
-git commit -m "Translations update"
-
-git push --recurse-submodules=on-demand
+Push-Location "$PSScriptRoot\..\.."
+& archi_core.ps1 --download
 pause
--- tools/crowdin-cli/archi_upload.ps1
+++ tools/crowdin-cli/archi_upload.ps1
@@ -2,26 +2,6 @@ Set-StrictMode -Version Latest
 $ErrorActionPreference = 'Stop'
 $ProgressPreference = 'SilentlyContinue'
 
-Set-Location $PSScriptRoot
-Set-Location ..\\..
-
-Push-Location -Path ASF-ui
-git reset --hard
-git clean -fd
-git pull
-Pop-Location
-
-Push-Location -Path ASF-WebConfigGenerator
-git reset --hard
-git clean -fd
-git pull
-Pop-Location
-
-Push-Location -Path wiki
-git reset --hard
-git clean -fd
-git pull
-Pop-Location
-
-crowdin -b master --identity tools\\crowdin-cli\\crowdin_identity.yml upload sources
+Push-Location "$PSScriptRoot\..\.."
+& archi_core.ps1 --upload
 pause
