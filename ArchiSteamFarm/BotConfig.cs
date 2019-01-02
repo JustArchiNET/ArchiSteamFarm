3bbddcf7238fc00c4927b913046ceb57214aca66
JustArchi <JustArchi@JustArchi.net>
2019-01-02 16:32:53 +0100

    Happy new year :tada:

--- ArchiSteamFarm/BotConfig.cs
+++ ArchiSteamFarm/BotConfig.cs
@@ -4,7 +4,7 @@
 //  / ___ \ | |  | (__ | | | || | ___) || |_|  __/| (_| || | | | | ||  _|| (_| || |   | | | | | |
 // /_/   \_\|_|   \___||_| |_||_||____/  \__|\___| \__,_||_| |_| |_||_|   \__,_||_|   |_| |_| |_|
 // 
-// Copyright 2015-2018 Łukasz "JustArchi" Domeradzki
+// Copyright 2015-2019 Łukasz "JustArchi" Domeradzki
 // Contact: JustArchi@JustArchi.net
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
