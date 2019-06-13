fefd7643258f1806d404476ba176dae8967cab32
JustArchi <JustArchi@JustArchi.net>
2019-06-13 16:31:39 +0200

    Closes #1277

--- ArchiSteamFarm/ArchiWebHandler.cs
+++ ArchiSteamFarm/ArchiWebHandler.cs
@@ -1975,6 +1975,12 @@ namespace ArchiSteamFarm {
 			WebBrowser.CookieContainer.Add(new Cookie("steamLoginSecure", steamLoginSecure, "/", "." + SteamCommunityHost));
 			WebBrowser.CookieContainer.Add(new Cookie("steamLoginSecure", steamLoginSecure, "/", "." + SteamStoreHost));
 
+			// Report proper time when doing timezone-based calculations, see setTimezoneCookies() from https://steamcommunity-a.akamaihd.net/public/shared/javascript/shared_global.js
+			string timeZoneOffset = DateTimeOffset.Now.Offset.TotalSeconds + ",0";
+
+			WebBrowser.CookieContainer.Add(new Cookie("timezoneOffset", timeZoneOffset, "/", "." + SteamCommunityHost));
+			WebBrowser.CookieContainer.Add(new Cookie("timezoneOffset", timeZoneOffset, "/", "." + SteamStoreHost));
+
 			Bot.ArchiLogger.LogGenericInfo(Strings.Success);
 
 			// Unlock Steam Parental if needed
