0d7a9c27e5a7c566c1de9a15c4e7707f16a6d610
JustArchi <JustArchi@JustArchi.net>
2019-07-02 15:28:37 +0200

    Misc

--- ArchiSteamFarm/Commands.cs
+++ ArchiSteamFarm/Commands.cs
@@ -1649,7 +1649,7 @@ namespace ArchiSteamFarm {
 
 						if ((packageIDs != null) && (packageIDs.Count > 0)) {
 							result.Add(entry);
-							response.AppendLine(FormatBotResponse(string.Format(Strings.BotOwnedAlready, entry)));
+							response.AppendLine(FormatBotResponse(string.Format(Strings.BotOwnedAlready, appID)));
 						} else {
 							if (gamesOwned == null) {
 								gamesOwned = await FetchGamesOwned().ConfigureAwait(false);
@@ -1663,9 +1663,9 @@ namespace ArchiSteamFarm {
 
 							if (gamesOwned.TryGetValue(appID, out string gameName)) {
 								result.Add(entry);
-								response.AppendLine(FormatBotResponse(string.Format(Strings.BotOwnedAlreadyWithName, entry, gameName)));
+								response.AppendLine(FormatBotResponse(string.Format(Strings.BotOwnedAlreadyWithName, appID, gameName)));
 							} else {
-								response.AppendLine(FormatBotResponse(string.Format(Strings.BotNotOwnedYet, entry)));
+								response.AppendLine(FormatBotResponse(string.Format(Strings.BotNotOwnedYet, appID)));
 							}
 						}
 
@@ -1695,11 +1695,11 @@ namespace ArchiSteamFarm {
 
 						bool foundWithRegex = false;
 
-						foreach (string gameName in gamesOwned.Values.Where(gameName => regex.IsMatch(gameName))) {
+						foreach ((uint appID, string gameName) in gamesOwned.Where(gameOwned => regex.IsMatch(gameOwned.Value))) {
 							foundWithRegex = true;
 
 							result.Add(entry);
-							response.AppendLine(FormatBotResponse(string.Format(Strings.BotOwnedAlreadyWithName, entry, gameName)));
+							response.AppendLine(FormatBotResponse(string.Format(Strings.BotOwnedAlreadyWithName, appID, gameName)));
 						}
 
 						if (!foundWithRegex) {
@@ -1711,9 +1711,9 @@ namespace ArchiSteamFarm {
 					case "sub" when uint.TryParse(game, out packageID) && (packageID > 0):
 						if (Bot.OwnedPackageIDs.ContainsKey(packageID)) {
 							result.Add(entry);
-							response.AppendLine(FormatBotResponse(string.Format(Strings.BotOwnedAlready, entry)));
+							response.AppendLine(FormatBotResponse(string.Format(Strings.BotOwnedAlready, packageID)));
 						} else {
-							response.AppendLine(FormatBotResponse(string.Format(Strings.BotNotOwnedYet, entry)));
+							response.AppendLine(FormatBotResponse(string.Format(Strings.BotNotOwnedYet, packageID)));
 						}
 
 						break;
@@ -1730,11 +1730,11 @@ namespace ArchiSteamFarm {
 
 						bool foundWithName = false;
 
-						foreach (string gameName in gamesOwned.Values.Where(gameName => gameName.IndexOf(game, StringComparison.OrdinalIgnoreCase) >= 0)) {
+						foreach ((uint appID, string gameName) in gamesOwned.Where(gameOwned => gameOwned.Value.IndexOf(game, StringComparison.OrdinalIgnoreCase) >= 0)) {
 							foundWithName = true;
 
 							result.Add(entry);
-							response.AppendLine(FormatBotResponse(string.Format(Strings.BotOwnedAlreadyWithName, entry, gameName)));
+							response.AppendLine(FormatBotResponse(string.Format(Strings.BotOwnedAlreadyWithName, appID, gameName)));
 						}
 
 						if (!foundWithName) {
