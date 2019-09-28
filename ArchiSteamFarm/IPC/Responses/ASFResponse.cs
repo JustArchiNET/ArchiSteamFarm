a49c72426cbc709190d740c96e5f538d80b031f6
JustArchi <JustArchi@JustArchi.net>
2019-09-28 17:16:42 +0200

    Always use string representation of the version

--- ArchiSteamFarm/IPC/Responses/ASFResponse.cs
+++ ArchiSteamFarm/IPC/Responses/ASFResponse.cs
@@ -59,7 +59,7 @@ namespace ArchiSteamFarm.IPC.Responses {
 		/// </summary>
 		[JsonProperty(Required = Required.Always)]
 		[Required]
-		public readonly Version Version;
+		public readonly string Version;
 
 		internal ASFResponse([NotNull] string buildVariant, [NotNull] GlobalConfig globalConfig, uint memoryUsage, DateTime processStartTime, [NotNull] Version version) {
 			if (string.IsNullOrEmpty(buildVariant) || (globalConfig == null) || (memoryUsage == 0) || (processStartTime == DateTime.MinValue) || (version == null)) {
@@ -70,7 +70,7 @@ namespace ArchiSteamFarm.IPC.Responses {
 			GlobalConfig = globalConfig;
 			MemoryUsage = memoryUsage;
 			ProcessStartTime = processStartTime;
-			Version = version;
+			Version = version.ToString();
 		}
 	}
 }
