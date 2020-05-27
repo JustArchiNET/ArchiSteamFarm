e0935f1e70eed83f80be46e480145c357311cfa1
JustArchi <JustArchi@JustArchi.net>
2020-05-27 10:30:47 +0200

    Update ASF.cs

--- ArchiSteamFarm/ASF.cs
+++ ArchiSteamFarm/ASF.cs
@@ -298,8 +298,13 @@ namespace ArchiSteamFarm {
 					return null;
 				}
 
-				// We disable ArchiKestrel here as the update process moves the core files and might result in IPC crash
-				await ArchiKestrel.Stop().ConfigureAwait(false);
+				try {
+					// We disable ArchiKestrel here as the update process moves the core files and might result in IPC crash
+					// TODO: It might fail if the update was triggered from the API, this should be something to improve in the future, by changing the structure into request -> return response -> finish update
+					await ArchiKestrel.Stop().ConfigureAwait(false);
+				} catch (Exception e) {
+					ArchiLogger.LogGenericWarningException(e);
+				}
 
 				try {
 #if !NETFRAMEWORK
