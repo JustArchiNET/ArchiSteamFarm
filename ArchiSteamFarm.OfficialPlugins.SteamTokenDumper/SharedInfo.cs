f80bf929f7c84031b6e1798c248f3dff88a73419
JustArchi <JustArchi@JustArchi.net>
2020-06-15 16:31:26 +0200

    Update SharedInfo.cs

--- ArchiSteamFarm.OfficialPlugins.SteamTokenDumper/SharedInfo.cs
+++ ArchiSteamFarm.OfficialPlugins.SteamTokenDumper/SharedInfo.cs
@@ -22,8 +22,8 @@
 namespace ArchiSteamFarm.OfficialPlugins.SteamTokenDumper {
 	internal static class SharedInfo {
 		internal const byte ApiVersion = 1;
-		internal const string ConfigurationPropertyEnabled = nameof(SteamTokenDumperPlugin) + "Enabled";
 		internal const byte AppInfosPerSingleRequest = byte.MaxValue;
+		internal const string ConfigurationPropertyEnabled = nameof(SteamTokenDumperPlugin) + "Enabled";
 		internal const byte MaximumHoursBetweenRefresh = 8; // Per single bot account, makes sense to be 2 or 3 times less than MinimumHoursBetweenUploads
 		internal const byte MaximumMinutesBeforeFirstUpload = 60; // Must be greater or equal to MinimumMinutesBeforeFirstUpload
 		internal const byte MinimumHoursBetweenUploads = 24;
