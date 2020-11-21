084388087e7ac844d5c2a4ff4c349a0d26cb390a
JustArchi <JustArchi@JustArchi.net>
2020-11-21 18:30:49 +0100

    Revert "Make --ignore-unsupported-environment emit a warning instead of nothing"
    This reverts commit dd1bb75b8ab69bdcbb5b42663d56b6903499341e.

--- ArchiSteamFarm/Program.cs
+++ ArchiSteamFarm/Program.cs
@@ -185,17 +185,13 @@ namespace ArchiSteamFarm {
 			Console.Title = SharedInfo.ProgramIdentifier;
 			ASF.ArchiLogger.LogGenericInfo(SharedInfo.ProgramIdentifier);
 
-			if (!OS.VerifyEnvironment()) {
-				string errorMessage = string.Format(CultureInfo.CurrentCulture, Strings.WarningUnsupportedEnvironment, SharedInfo.BuildInfo.Variant, OS.Variant);
-
-				if (!IgnoreUnsupportedEnvironment) {
-					ASF.ArchiLogger.LogGenericError(errorMessage);
+			if (!IgnoreUnsupportedEnvironment) {
+				if (!OS.VerifyEnvironment()) {
+					ASF.ArchiLogger.LogGenericError(string.Format(CultureInfo.CurrentCulture, Strings.WarningUnsupportedEnvironment, SharedInfo.BuildInfo.Variant, OS.Variant));
 					await Task.Delay(10000).ConfigureAwait(false);
 
 					return false;
 				}
-
-				ASF.ArchiLogger.LogGenericWarning(errorMessage);
 			}
 
 			return true;
