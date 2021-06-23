08405fb21135ef0a8d12c19ff62ea4ea63bd4f0b
Vitaliya Chumakova <vichuma9@gmail.com>
2021-06-23 04:05:32 +0300

    Call global config init for plugins after setting global variable

--- ArchiSteamFarm/Core/ASF.cs
+++ ArchiSteamFarm/Core/ASF.cs
@@ -151,6 +151,8 @@ namespace ArchiSteamFarm.Core {
 
 			GlobalConfig = globalConfig;
 
+			await PluginsCore.OnASFInitModules(globalConfig.AdditionalProperties).ConfigureAwait(false);
+
 			// The only purpose of using hashingAlgorithm below is to cut on a potential size of the resource name - paths can be really long, and we almost certainly have some upper limit on the resource name we can allocate
 			// At the same time it'd be the best if we avoided all special characters, such as '/' found e.g. in base64, as we can't be sure that it's not a prohibited character in regards to native OS implementation
 			// Because of that, SHA256 is sufficient for our case, as it generates alphanumeric characters only, and is barely 256-bit long. We don't need any kind of complex cryptography or collision detection here, any hashing algorithm will do, and the shorter the better
--- ArchiSteamFarm/Program.cs
+++ ArchiSteamFarm/Program.cs
@@ -286,8 +286,6 @@ namespace ArchiSteamFarm {
 				ASF.ArchiLogger.LogGenericInfo(Strings.Done);
 			}
 
-			await PluginsCore.OnASFInitModules(globalConfig.AdditionalProperties).ConfigureAwait(false);
-
 			await ASF.InitGlobalConfig(globalConfig).ConfigureAwait(false);
 
 			// Skip translation progress for English and invariant (such as "C") cultures
