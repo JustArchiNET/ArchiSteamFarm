1a3b4362166bf70dcffc5889f1555837753c16f0
Archi <JustArchi@JustArchi.net>
2021-07-03 14:25:13 +0200

    Mis

--- ArchiSteamFarm/Helpers/CrossProcessFileBasedSemaphore.cs
+++ ArchiSteamFarm/Helpers/CrossProcessFileBasedSemaphore.cs
@@ -184,9 +184,7 @@ namespace ArchiSteamFarm.Helpers {
 			}
 
 			try {
-#pragma warning disable CA1508 // False positive, FileStream is not null here indeed, but using clause is needed for dispose
-				using (new FileStream(FilePath, FileMode.CreateNew)) { }
-#pragma warning restore CA1508 // False positive, FileStream is not null here indeed, but using clause is needed for dispose
+				new FileStream(FilePath, FileMode.CreateNew).Dispose();
 
 				if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
 					FileInfo fileInfo = new(FilePath);
