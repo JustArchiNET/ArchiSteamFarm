566be6e8c4b7f761f59d7b552b5ca8494e7547cd
Archi <JustArchi@JustArchi.net>
2021-11-10 19:39:04 +0100

    Use --self-contained and --no-self-contained as recommended by .NET 6.0 docs

--- cc.sh
+++ cc.sh
@@ -111,7 +111,7 @@ if [ "$ASF_UI" -eq 1 ]; then
 	fi
 fi
 
-DOTNET_FLAGS="-c $CONFIGURATION -f $TARGET_FRAMEWORK -p:ContinuousIntegrationBuild=true -p:SelfContained=false -p:UseAppHost=false -r ${os_type}-${cpu_architecture} --nologo"
+DOTNET_FLAGS="-c $CONFIGURATION -f $TARGET_FRAMEWORK -p:ContinuousIntegrationBuild=true -p:UseAppHost=false -r ${os_type}-${cpu_architecture} --nologo --no-self-contained"
 
 if [ "$ANALYSIS" -eq 0 ]; then
 	DOTNET_FLAGS="$DOTNET_FLAGS -p:AnalysisMode=AllDisabledByDefault"
