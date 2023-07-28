35364e46caeeb9f10e192c09c888d71d6252351c
Archi <JustArchi@JustArchi.net>
2023-07-28 11:46:23 +0200

    Closes #2972

--- ArchiSteamFarm/Steam/Bot.cs
+++ ArchiSteamFarm/Steam/Bot.cs
@@ -2054,6 +2054,7 @@ public sealed class Bot : IAsyncDisposable, IDisposable {
 			case EResult.AccountLoginDeniedNeedTwoFactor:
 			case EResult.AccountLoginDeniedThrottle:
 			case EResult.DuplicateRequest: // This will happen if user reacts to popup and tries to use the code afterwards, we have the code saved in ASF, we just need to try again
+			case EResult.Expired: // Same as Timeout
 			case EResult.FileNotFound: // User denied approval despite telling us that he accepted it, just try again
 			case EResult.InvalidPassword:
 			case EResult.NoConnection:
