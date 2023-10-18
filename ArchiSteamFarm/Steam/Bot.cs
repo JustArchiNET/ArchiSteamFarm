d44c57a48f1f6ff5a86a0d6105e81a1677392420
Archi <JustArchi@JustArchi.net>
2023-10-18 10:56:07 +0200

    Misc

--- ArchiSteamFarm/Steam/Bot.cs
+++ ArchiSteamFarm/Steam/Bot.cs
@@ -29,7 +29,6 @@ using System.ComponentModel;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Security.Cryptography;
 using System.Text.RegularExpressions;
