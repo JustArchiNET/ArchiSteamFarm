4ab7ec187fc09e232e88316ae08bdafa9450b388
Archi <JustArchi@JustArchi.net>
2024-02-12 21:43:20 +0100

    Optimize our json utilities even further

--- ArchiSteamFarm/Helpers/Json/JsonUtilities.cs
+++ ArchiSteamFarm/Helpers/Json/JsonUtilities.cs
@@ -51,15 +51,20 @@ public static class JsonUtilities {
 		bool potentialDisallowedNullsPossible = false;
 
 		foreach (JsonPropertyInfo property in jsonTypeInfo.Properties) {
-			if (!potentialDisallowedNullsPossible && (property.Get != null) && (property.AttributeProvider?.IsDefined(typeof(JsonDisallowNullAttribute), false) == true)) {
+			// All our modifications require a valid Get method on a property
+			if (property.Get == null) {
+				continue;
+			}
+
+			// The object should be validated against potential nulls if at least one property has [JsonDisallowNullAttribute] declared, avoid performance penalty otherwise
+			if (!potentialDisallowedNullsPossible && (property.AttributeProvider?.IsDefined(typeof(JsonDisallowNullAttribute), false) == true)) {
 				potentialDisallowedNullsPossible = true;
 			}
 
-			property.ShouldSerialize = (parent, _) => {
-				bool? shouldSerialize = ShouldSerialize(parent, property);
-
-				return shouldSerialize ?? true;
-			};
+			// The property should be checked against ShouldSerialize if there is a valid method to invoke, avoid performance penalty otherwise
+			if (GetShouldSerializeMethod(jsonTypeInfo.Type, property) != null) {
+				property.ShouldSerialize = (parent, _) => ShouldSerialize(parent, property);
+			}
 		}
 
 		if (potentialDisallowedNullsPossible) {
@@ -67,7 +72,47 @@ public static class JsonUtilities {
 		}
 	}
 
-	[UnconditionalSuppressMessage("AssemblyLoadTrimming", "IL2075", Justification = "We don't care about trimmed properties, it's not like we can make it work differently")]
+	[UnconditionalSuppressMessage("AssemblyLoadTrimming", "IL2070", Justification = "We don't care about trimmed methods, it's not like we can make it work differently anyway")]
+	[UnconditionalSuppressMessage("AssemblyLoadTrimming", "IL2075", Justification = "We don't care about trimmed properties, it's not like we can make it work differently anyway")]
+	private static MethodInfo? GetShouldSerializeMethod([SuppressMessage("ReSharper", "SuggestBaseTypeForParameter")] Type parent, JsonPropertyInfo property) {
+		ArgumentNullException.ThrowIfNull(parent);
+		ArgumentNullException.ThrowIfNull(property);
+
+		// Handle most common case where ShouldSerializeXYZ() matches property name
+		MethodInfo? result = parent.GetMethod($"ShouldSerialize{property.Name}", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static, null, Type.EmptyTypes, null);
+
+		if (result?.ReturnType == typeof(bool)) {
+			// Method exists and returns a boolean, that's what we'd like to hear
+			return result;
+		}
+
+		// Handle less common case where ShouldSerializeXYZ() matches original member name
+		PropertyInfo? memberNameProperty = property.GetType().GetProperty("MemberName", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+		if (memberNameProperty == null) {
+			// Should never happen, investigate if it does
+			throw new InvalidOperationException(nameof(memberNameProperty));
+		}
+
+		object? memberNameResult = memberNameProperty.GetValue(property);
+
+		if (memberNameResult is not string memberName) {
+			// Should never happen, investigate if it does
+			throw new InvalidOperationException(nameof(memberName));
+		}
+
+		if (string.IsNullOrEmpty(memberName)) {
+			// We don't have anything to work with further, there is no ShouldSerialize() method
+			return null;
+		}
+
+		result = parent.GetMethod($"ShouldSerialize{memberName}", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static, null, Type.EmptyTypes, null);
+
+		// Use alternative method if it exists and returns a boolean
+		return result?.ReturnType == typeof(bool) ? result : null;
+	}
+
+	[UnconditionalSuppressMessage("AssemblyLoadTrimming", "IL2075", Justification = "We don't care about trimmed properties, it's not like we can make it work differently anyway")]
 	private static void OnPotentialDisallowedNullsDeserialized(object obj) {
 		ArgumentNullException.ThrowIfNull(obj);
 
@@ -76,65 +121,31 @@ public static class JsonUtilities {
 		}
 	}
 
-	[UnconditionalSuppressMessage("AssemblyLoadTrimming", "IL2075", Justification = "We don't care about trimmed properties, it's not like we can make it work differently")]
-	private static bool? ShouldSerialize(object parent, JsonPropertyInfo property) {
+	private static bool ShouldSerialize(object parent, JsonPropertyInfo property) {
 		ArgumentNullException.ThrowIfNull(parent);
 		ArgumentNullException.ThrowIfNull(property);
 
 		try {
-			// Handle most common case where ShouldSerializeXYZ() matches property name
-			bool? shouldSerialize = ShouldSerialize(parent, property.Name);
+			MethodInfo? shouldSerializeMethod = GetShouldSerializeMethod(parent.GetType(), property);
 
-			if (shouldSerialize.HasValue) {
-				return shouldSerialize.Value;
-			}
-
-			// Handle less common case where ShouldSerializeXYZ() matches original member name
-			PropertyInfo? memberNameProperty = property.GetType().GetProperty("MemberName", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
-
-			if (memberNameProperty == null) {
-				// Should never happen, investigate if it does
-				throw new InvalidOperationException(nameof(memberNameProperty));
-			}
-
-			object? memberNameResult = memberNameProperty.GetValue(property);
-
-			if (memberNameResult is not string memberName) {
-				// Should never happen, investigate if it does
-				throw new InvalidOperationException(nameof(memberName));
-			}
-
-			return !string.IsNullOrEmpty(memberName) ? ShouldSerialize(parent, memberName) : null;
-		} catch (Exception e) {
-			ASF.ArchiLogger.LogGenericException(e);
-
-			return null;
-		}
-	}
-
-	[UnconditionalSuppressMessage("AssemblyLoadTrimming", "IL2075", Justification = "We don't care about trimmed methods, it's not like we can make it work differently")]
-	private static bool? ShouldSerialize(object parent, string propertyName) {
-		ArgumentNullException.ThrowIfNull(parent);
-		ArgumentException.ThrowIfNullOrEmpty(propertyName);
-
-		try {
-			MethodInfo? shouldSerializeMethod = parent.GetType().GetMethod($"ShouldSerialize{propertyName}", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static, null, Type.EmptyTypes, null);
-
-			if ((shouldSerializeMethod == null) || (shouldSerializeMethod.ReturnType != typeof(bool))) {
-				return null;
+			if (shouldSerializeMethod == null) {
+				// Should not happen, we've already determined we have a method to call
+				throw new InvalidOperationException(nameof(shouldSerializeMethod));
 			}
 
 			object? shouldSerialize = shouldSerializeMethod.Invoke(parent, null);
 
-			if (shouldSerialize is bool result) {
-				return result;
+			if (shouldSerialize is not bool result) {
+				// Should not happen, we've already determined we have a method that returns a boolean
+				throw new InvalidOperationException(nameof(shouldSerialize));
 			}
 
-			return null;
+			return result;
 		} catch (Exception e) {
+			// Fallback to always serialize, we can't do much more anyway
 			ASF.ArchiLogger.LogGenericException(e);
 
-			return null;
+			return true;
 		}
 	}
 }
