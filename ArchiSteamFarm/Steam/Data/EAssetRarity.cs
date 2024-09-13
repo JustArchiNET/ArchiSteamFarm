12d46489b04ae4bc0b7d6d02548be326e6f7d27e
dmitz <dmitz@disroot.org>
2024-09-13 09:57:29 +0300

    Remove trailing comment

--- ArchiSteamFarm/Steam/Data/EAssetRarity.cs
+++ ArchiSteamFarm/Steam/Data/EAssetRarity.cs
@@ -20,7 +20,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//
 
 namespace ArchiSteamFarm.Steam.Data;
 
