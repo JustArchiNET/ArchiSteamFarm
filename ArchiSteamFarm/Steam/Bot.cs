c22eb5872ebcd1a284e354b645d4dc634412513b
Łukasz Domeradzki <JustArchi@JustArchi.net>
2024-10-14 20:07:23 +0200

    Make it work

--- ArchiSteamFarm/Steam/Bot.cs
+++ ArchiSteamFarm/Steam/Bot.cs
@@ -2098,6 +2098,7 @@ public sealed class Bot : IAsyncDisposable, IDisposable {
 	}
 
 	private void DisposeShared() {
+		ArchiHandler.Dispose();
 		ArchiWebHandler.Dispose();
 		BotDatabase.Dispose();
 		ConnectionSemaphore.Dispose();
