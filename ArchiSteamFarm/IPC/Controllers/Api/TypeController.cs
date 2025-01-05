9e207025c5726689bd3b487b782bcd91237f23d9
Łukasz Domeradzki <JustArchi@JustArchi.net>
2025-01-05 02:40:56 +0100

    Happy new year

--- ArchiSteamFarm/IPC/Controllers/Api/TypeController.cs
+++ ArchiSteamFarm/IPC/Controllers/Api/TypeController.cs
@@ -6,7 +6,7 @@
 // /_/   \_\|_|   \___||_| |_||_||____/  \__|\___| \__,_||_| |_| |_||_|   \__,_||_|   |_| |_| |_|
 // ----------------------------------------------------------------------------------------------
 // |
-// Copyright 2015-2024 Łukasz "JustArchi" Domeradzki
+// Copyright 2015-2025 Łukasz "JustArchi" Domeradzki
 // Contact: JustArchi@JustArchi.net
 // |
 // Licensed under the Apache License, Version 2.0 (the "License");
