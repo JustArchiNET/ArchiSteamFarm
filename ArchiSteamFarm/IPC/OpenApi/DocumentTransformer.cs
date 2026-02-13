330a5c7b76672fbd1301d45be29516954b176794
Łukasz Domeradzki <JustArchi@JustArchi.net>
2026-02-13 12:23:45 +0100

    Happy new year

--- ArchiSteamFarm/IPC/OpenApi/DocumentTransformer.cs
+++ ArchiSteamFarm/IPC/OpenApi/DocumentTransformer.cs
@@ -6,7 +6,7 @@
 // /_/   \_\|_|   \___||_| |_||_||____/  \__|\___| \__,_||_| |_| |_||_|   \__,_||_|   |_| |_| |_|
 // ----------------------------------------------------------------------------------------------
 // |
-// Copyright 2015-2025 Łukasz "JustArchi" Domeradzki
+// Copyright 2015-2026 Łukasz "JustArchi" Domeradzki
 // Contact: JustArchi@JustArchi.net
 // |
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -83,7 +83,6 @@ internal sealed class DocumentTransformer : IOpenApiDocumentTransformer {
 							Description = nameof(HttpStatusCode.SwitchingProtocols)
 						}
 					},
-
 					{
 						StatusCodes.Status200OK.ToString(CultureInfo.InvariantCulture),
 
@@ -91,7 +90,6 @@ internal sealed class DocumentTransformer : IOpenApiDocumentTransformer {
 							Description = nameof(HttpStatusCode.OK)
 						}
 					},
-
 					{
 						StatusCodes.Status400BadRequest.ToString(CultureInfo.InvariantCulture),
 
