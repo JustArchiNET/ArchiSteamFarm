cf6b70b02766b81af2e5a8c8444a7e01ab2e6170
Łukasz Domeradzki <JustArchi@JustArchi.net>
2026-09-09 22:22:14 +0200

    Add extra logging for STM

--- ArchiSteamFarm.OfficialPlugins.ItemsMatcher/RemoteCommunication.cs
+++ ArchiSteamFarm.OfficialPlugins.ItemsMatcher/RemoteCommunication.cs
@@ -347,6 +347,10 @@ internal sealed class RemoteCommunication : IAsyncDisposable, IDisposable {
 					ShouldSendAnnouncementEarlier = false;
 					ShouldSendHeartBeats = true;
 
+					// Keep informational logging for the user though, even if the real request is not sent
+					Bot.ArchiLogger.LogGenericInfo(Localization.Strings.FormatListingAnnouncing(Bot.SteamID, nickname ?? Bot.SteamID.ToString(CultureInfo.InvariantCulture), assetsForListing.Count));
+					Bot.ArchiLogger.LogGenericInfo(Strings.Success);
+
 					if (triggerImmediately) {
 						Utilities.InBackground(() => OnHeartBeatTimer());
 					}
